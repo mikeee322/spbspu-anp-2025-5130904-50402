94608bc1d54de309a673ee9802a0a4b1c9b0c2ad
Михаил Лачугин <mikee@192-168-1-50.local>
2025-11-15 02:56:10 +0300

    add task 2, begin staticmtx and fix some staff

--- lachugin.mikhail/P3/main.cpp
+++ lachugin.mikhail/P3/main.cpp
@@ -1,22 +1,47 @@
 #include <iostream>
 #include <fstream>
 namespace lachugin {
+  int *convert(const int **a, size_t r, size_t c);
+  double circle( int **mtx,long long i, long long j);
+  int **dynmtx(int r, int c);
+  int **staticmtx(size_t rows);
+  int *LFT_BOT_CLK (int **mtx, size_t rows, size_t cols);
+  int *BLT_SMT_MTR (int **mtx, size_t rows, size_t cols);
+  int **make(std::ifstream& fin,size_t rows, size_t cols, int prmt);
 
-  int **make(int rows, int cols);
+  int *convert(int **a, size_t r, size_t c) {
 
-  int **make(int r, int c)
-  {
+    int *result = new int[r*c];
+    size_t start = 0;
+    while (start < r * c) {
+      for (size_t i = start; i < start + c; i++) {
+        for (size_t j = 0; j < c; j++) {
+          result [start] = a[i][j];
+          start++;
+        }
+      }
+    }
+    return result;
+  }
+
+
+
+  int **dynmtx(size_t r, size_t c) {
     int **mtx = new int *[r];
     for (size_t i = 0; i < r; i++){
       mtx[i] = new int[c];
-
     }
     return mtx;
   }
 
-  int **dynmtx(std::ifstream& fin,size_t rows, size_t cols) {
+  int **make(std::ifstream& fin,size_t rows, size_t cols, int prmt) {
     int **result = nullptr;
-    result = make(rows, cols);
+    if (prmt == 1) {
+      result = staticmtx(rows);
+    }
+    if (prmt == 2) {
+      result = dynmtx(rows, cols);
+    }
     for (size_t i = 0; i < rows; i++) {
       for (size_t j = 0; j < cols; j++) {
         fin >> result[i][j];
@@ -26,39 +51,92 @@ namespace lachugin {
     return result;
   };
 
+  int **staticmtx(size_t rows) {
+    int mtx[100][100];
+    int *row_ptrs[rows];
+    for(int i = 0; i < rows; i++) {
+      row_ptrs[i] = mtx[i];
+    }
+    return row_ptrs;
+
+  }
+
   int *LFT_BOT_CLK (int **mtx, size_t rows, size_t cols) {
     size_t d = 1;
     size_t k = 0;
     size_t n = 0;
     while (d < rows * cols) {
-      for (int i = (rows-1)-n; i >= 0+n; i--) {   // up
+      for (unsigned long long i = (rows-1)-n; i >= 0+n; i--) {   // up
         mtx[i][0+n] -= d;
         d++;
       }
-
+      if (d == rows * cols) {
+        break;
+      }
       k++;
-
-
-      for (int i = 0+k; i < cols-n; i++) { // right
+      for (unsigned long long i = 0+k; i < cols-n; i++) { // right
         mtx[0+n][i] -= d;
         d++;
       }
-
-      for (int i = 0+k; i < rows-n; i++) {   // down
+      if (d == rows * cols) {
+        break;
+      }
+      for (unsigned long long  i= 0+k; i < rows-n; i++) {   // down
         mtx[i][0-n] -= d;
         d++;
       }
-
-
+      if (d == rows * cols) {
+        break;
+      }
       n++;
-
-      for (int i = (cols-1)-n; i >= 0+n; i--) {   // left
+      for (unsigned long long  i = (cols-1)-n; i >= 0+n; i--) {   // left
         mtx[(rows-1)-n][i] -= d;
         d++;
       }
     }
+    int *result = nullptr;
+    result = convert(mtx, rows, cols);
+    return result;
   }
 
+  int *BLT_SMT_MTR (int **mtx, size_t rows, size_t cols) {
+    for (size_t i = 0; i < rows; i++) {
+      for (size_t j = 0; j < cols; j++) {
+        mtx[i][j] = circle(mtx, i, j);
+      }
+    }
+    int *result = nullptr;
+    result = convert(mtx, rows, cols);
+    return result;
+  }
+
+  double circle(int **mtx,long long i, long long j) {
+    size_t k = 0;
+    size_t sum = 0;
+    if (i-1 >= 0&& j-1 >= 0) {
+      k++;
+      sum += mtx[i-1][j-1];
+    } else if (j-1 >= 0) {
+      k++;
+      sum += mtx[i][j-1];
+    } else if (i+1 >=0 && j-1 >= 0) {
+      k++;
+      sum += mtx[i+1][j-1];
+    } else if (i+1 >= 0) {
+      k++;
+      sum += mtx[i+1][j];
+    } else if (i+1 >= 0 && j+1 >= 0) {
+      k++;
+      sum += mtx[i+1][j+1];
+    } else if (j+1 >= 0) {
+      k++;
+      sum += mtx[i][j+1];
+    } else if (j+1 >= 0 && i-1 >= 0) {
+      k++;
+      sum += mtx[i-1][j+1];
+    }
+    return static_cast<double>(sum)/static_cast<double>(k);
+  }
 
 
 
@@ -100,9 +178,17 @@ int main(int argc, char ** argv) {
     return 1;
   }
   int *var1 = nullptr;
+  int *var2 = nullptr;
   if (prmt == 2) {
     int **mtx = nullptr;
-    mtx = lachugin::dynmtx(fin, rows, cols);
-
+    mtx = lachugin::make(fin, rows, cols, prmt);
+    var1 = lachugin::LFT_BOT_CLK(mtx, rows, cols);
+    var2 = lachugin::BLT_SMT_MTR(mtx, rows, cols);
+  }
+  if (prmt == 1) {
+    int **mtx = nullptr;
+    mtx = lachugin::make(fin, rows, cols, prmt);
+    var1 = lachugin::LFT_BOT_CLK(mtx, rows, cols);
+    var2 = lachugin::BLT_SMT_MTR(mtx, rows, cols);
   }
 }
